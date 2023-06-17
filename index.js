af9f6bab9b87b6df7c795707df64805a12fe4ece
Latrell Page <pagelatrell14@gmail.com>
2023-06-17 15:17:53 -0500

    Added github username and license list

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
 
 const questions = [
     {
@@ -41,9 +38,16 @@ const questions = [
         message: 'Are you open to contributions from other developers? If so, what guidelines or instructions should they follow?',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'Do you have a specific license for your project? If yes, which one?',
+        message: 'Choose a license.',
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'GNU General Public License (GPL) v3',
+            'Creative Commons Attribution 4.0 International (CC BY 4.0)',
+            'The Unlicense'
+        ]
     },
     {
         type: 'input',
@@ -53,17 +57,20 @@ const questions = [
     {
         type: 'input',
         name: 'contact',
-        message: 'If users have questions or need support, how can they reach out to you or your team?',
+        message: 'Please enter your email address.',
+    },
+    {
+        type: 'input',
+        name: 'contact2',
+        message: 'Please enter your GitHub username.',
     },
 ];
-
-
 
 function writeToFile(fileName, data) {
     inquirer
         .prompt(questions)
         .then(answers => {
-            const content = generateReadmeContent(answers); 
+            const content = generateReadmeContent(answers);
             fs.writeFile(fileName, content, err => {
                 if (err) {
                     console.error('An error occurred while writing the file:', err);
@@ -78,46 +85,40 @@ function writeToFile(fileName, data) {
 }
 
 function generateReadmeContent(answers) {
-    
     const content = `
-  # ${answers.title}
+      # ${answers.title}
   
-  ## Description
-  ${answers.description}
+      ## Description
+      ${answers.description}
   
-  ## Installation
-  ${answers.installation}
-
-  ## Usage
-  ${answers.usage}
-  ${answers.screenshots}
-
-  ## Features
-  ${answers.features}
-
-  ## Contribute
-  ${answers.contributing}
-
-  ## License
-  ${answers.license}
-
-  ## Author
-  ${answers.author}
-
-  ## Contact
-  ${answers.contact}
-
-
+      ## License
+      ${answers.license}
   
-  `;
+      ## Installation
+      ${answers.installation}
+  
+      ## Usage
+      ${answers.usage}
+      ${answers.screenshots}
+  
+      ## Features
+      ${answers.features}
+  
+      ## Contribute
+      ${answers.contributing}
+  
+      ## Questions
+      For additional questions, you can find my GitHub profile [here](https://github.com/${answers.contact2}).
+      You can also reach me by email at ${answers.contact}.
+    `;
+
     return content;
 }
-
 
 function init() {
     writeToFile('./dist/README.md', {});
 }
 
-
 init();
+
 
